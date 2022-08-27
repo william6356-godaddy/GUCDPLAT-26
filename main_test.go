cb8e79a5d968806fb1fc374bfa2c6490080048b5
william6356 <william6356@godaddy.com>
2022-08-27 11:19:38 -0700

    workflow commit

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 func TestAdd(t *testing.T) {
         want := 5
         got := Add(2, 3)
-        if got != want (
+        if got != want {
         t.Fatalf("wanted %d got %d", want, got)
-      )
+      }
 }
