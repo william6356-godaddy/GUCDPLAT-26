ec8aa7f9b5b4060393d95400523ba36e2777aa37
william6356 <william6356@godaddy.com>
2022-08-27 11:00:05 -0700

    workflow commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() (
+        fmt.Println("vim-go")
+)
+
+func Add(x, y int) int(
+    return x+y
+)
---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+        "testing"
+)
+
+func TestAdd(t * testing.T)(
+        want := 5
+        got := Add(2,3)
+        if got != want (
+        t.Fatalf("wanted %d got %d", want, got)
+      )
+)
